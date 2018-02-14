828d922d6d3cb2dc49f8b97b87427a0c513f9bbc
Someillan <Andrew.Someillan@wolterskluwer.com>
2018-02-14 14:26:41 -0500

    7

---
+++ src/app/payees/payees-filter.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'payees-filter',
+  templateUrl: './payees-filter.component.html',
+  styles: []
+})
+export class PayeesFilterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/payees/payees.module.ts
+++ src/app/payees/payees.module.ts
@@ -5,13 +5,14 @@ import { HttpClientModule } from '@angular/common/http';
 import { PayeesManagerComponent } from './payees-manager.component';
 import { PayeesListComponent } from './payees-list.component';
 import { PayeesDaoService } from './payees-dao.service';
+import { PayeesFilterComponent } from './payees-filter.component';
 
 @NgModule({
   imports: [
     CommonModule,
     HttpClientModule
   ],
-  declarations: [PayeesManagerComponent, PayeesListComponent],
+  declarations: [PayeesManagerComponent, PayeesListComponent, PayeesFilterComponent],
   exports: [PayeesManagerComponent],
   providers: [PayeesDaoService]
 })
